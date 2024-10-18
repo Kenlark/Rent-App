a1edfde4b38e0eec54512d0791e99bde4850a023
Kenlark <kenzokerachi@hotmail.fr>
2024-10-18 12:11:13 +0200

    implement lenis in the app for smooth scrolling

--- Rent-App/Client/src/App.jsx
+++ Rent-App/Client/src/App.jsx
@@ -1,6 +1,26 @@
 import Starter from "../src/index.jsx";
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1,
+      smooth: true,
+    });
+
+    const raf = (time) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(raf);
+    };
+  }, []);
+
   return (
     <>
       <Starter />
