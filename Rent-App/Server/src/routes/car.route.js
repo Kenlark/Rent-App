aa7221a23728ab2eebf05a4f12167fb5526b25b9
Kenlark <kenzokerachi@hotmail.fr>
2024-10-28 08:58:20 +0100

    fix minor bug in car.route.js

--- Rent-App/Server/src/routes/car.route.js
+++ Rent-App/Server/src/routes/car.route.js
@@ -7,7 +7,7 @@ import upload from "../middlewares/multer.middleware.js";
 const router = express.Router();
 
 router.post(
-  "./",
+  "/",
   authenticateUser,
   checkAdmin,
   upload.array("images"),
