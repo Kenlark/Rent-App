aaa03d792f9f82ed9527a60bf12d09490503fcac
Kenlark <kenzokerachi@hotmail.fr>
2024-12-11 14:49:50 +0100

    change param for the rate limit

--- Rent-App/Server/src/middlewares/rate-limit.middleware.js
+++ Rent-App/Server/src/middlewares/rate-limit.middleware.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 const apiRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 100,
+  limit: 1000,
   message: "Trop de requêtes de cette IP, veuillez réessayer plus tard",
   standardHeaders: true,
   legacyHeaders: false,
